934fb5eb397bb5d7030a4d620b63fb5bc42136e3
fatmajallouli-design <fatma.jallouli@insat.ucar.tn>
2026-01-21 17:52:28 +0100

    Update Vente.java

--- java/src/model/Vente.java
+++ java/src/model/Vente.java
@@ -4,20 +4,30 @@ import java.sql.Date;
 
 public class Vente {
 
-   
+    private int IdVente;
     private Date dateVente;
     private int quantite;
     private double montantTotal;
     private int idClient;
-    private int idProduit;
+    private String nom_medicamment;
 
-    Vente(Date dateVente, int quantite, double montantTotal, int idClient, int idProduit){
+    Vente(Date dateVente, int quantite, double montantTotal, int idClient, String nom_medicamment){
         this.dateVente=dateVente;
         this.quantite=quantite;
         this.montantTotal;
         this.idClient=idClient;
-        this.idProduit=idProduit;
+        this.nom_medicamment=nom_medicamment;
+
+       Vente(int IdVente,Date dateVente, int quantite, double montantTotal, int idClient, String nom_medicamment){
+        this.IdVente=IdVente;
+        this.dateVente=dateVente;
+        this.quantite=quantite;
+        this.montantTotal;
+        this.idClient=idClient;
+        this.nom_medicamment=nom_medicamment;
     }
+   public int getIdVente(){
+      return IdVente;}
 
     public void setIdVente(int idVente) {
         this.idVente = idVente;
@@ -55,11 +65,5 @@ public class Vente {
         this.idClient = idClient;
     }
 
-    public int getIdProduit() {
-        return idProduit;
-    }
-
-    public void setIdProduit(int idProduit) {
-        this.idProduit = idProduit;
-    }
+    
 }
