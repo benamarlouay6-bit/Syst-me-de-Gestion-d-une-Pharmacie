c53b0ecf3840d0106f70551447244e2005ea7c46
fatmajallouli-design <fatma.jallouli@insat.ucar.tn>
2026-01-21 13:24:11 +0100

    Update Client.java

--- java/src/model/Client.java
+++ java/src/model/Client.java
@@ -1,20 +1,19 @@
 package model;
 
 public class Client {
-    private int idClient;
+    
     private String nom;
     private String telephone;
     private String cin;
-    public Client(int idClient,String nom,String telephone,String cin)
+    public Client(String nom,String telephone,String cin)
     {
-        this.idClient=idCleint;
+        
         this.nom=nom;
         this.telephone=telephone;
         this.cin=cin;
     }
 
-    public int getIdClient() { return idClient; }
-    public void setIdClient(int idClient) { this.idClient = idClient; }
+   
 
     public String getNom() { return nom; }
     public void setNom(String nom) { this.nom = nom; }
