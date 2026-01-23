e6f6be6f2589a956afbd5b5a3d10c0b8883d9275
eyaabbes <eya.abbes@insat.ucar.tn>
2026-01-23 01:58:06 +0100

    Update Client.java

--- java/src/model/Client.java
+++ java/src/model/Client.java
@@ -1,26 +1,44 @@
 package model;
 
 public class Client {
-    
+
+    private int idClient;
     private String nom;
     private String telephone;
     private String cin;
-    public Client(String nom,String telephone,String cin)
-    {
-        
-        this.nom=nom;
-        this.telephone=telephone;
-        this.cin=cin;
+
+    public Client(int idClient, String nom, String telephone, String cin) {
+        this.idClient = idClient;
+        this.nom = nom;
+        this.telephone = telephone;
+        this.cin = cin;
     }
 
-   
+    public int getIdClient() {
+        return idClient;
+    }
 
-    public String getNom() { return nom; }
-    public void setNom(String nom) { this.nom = nom; }
+    public String getNom() {
+        return nom;
+    }
 
-    public String getTelephone() { return telephone; }
-    public void setTelephone(String telephone) { this.telephone = telephone; }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
-    public String getCin() { return cin; }
-    public void setCin(String cin) { this.cin = cin; }
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getCin() {
+        return cin;
+    }
+
+    public void setCin(String cin) {
+        this.cin = cin;
+    }
 }
