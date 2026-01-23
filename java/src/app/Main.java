ca376acab1718063c80c193a842e062b0c7619d4
eyaabbes <eya.abbes@insat.ucar.tn>
2026-01-23 01:50:12 +0100

    Update Main.java

--- java/src/app/Main.java
+++ java/src/app/Main.java
@@ -1,5 +1,17 @@
-package app;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import ui.view.LoginView;
 
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        System.out.println(" Application démarrée");
+        new LoginView().start(stage);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
