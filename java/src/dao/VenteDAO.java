145cb83b8d6af3b70237ed3b7a3a1c8c20f73bc7
eyaabbes <eya.abbes@insat.ucar.tn>
2026-01-23 01:53:49 +0100

    Update VenteDAO.java

--- java/src/dao/VenteDAO.java
+++ java/src/dao/VenteDAO.java
@@ -1,121 +1,103 @@
 package dao;
 
-import model.Vente;
 import ui.utils.DatabaseConnection;
+import exception.StockInsuffisantException;
+import model.Produit;
 
 import java.sql.*;
 
-import exception.StockInsuffisantException;
-
 public class VenteDAO {
+	public ResultSet getAllVentes() throws SQLException {
 
-	public void enregistrerVente(
-	        Date dateVente,
-	        String nomMedicament,
-	        int quantite,
-	        String cinClient
-	) {
+	    String sql =
+	            "SELECT idVente, dateVente, quantite, montantTotal, " +
+	            "nom_medicamment, idClient " +
+	            "FROM vente ORDER BY dateVente DESC";
 
-	    String checkClient =
-	            "SELECT idClient FROM client WHERE cin = ?";
+	    Connection con = DatabaseConnection.getConnection();
+	    Statement st = con.createStatement();
 
-	    String insertClient =
-	            "INSERT INTO client (nom, cin) VALUES (?, ?)";
-
-	    String checkProduit =
-	            "SELECT prixC, quantiteStock, seuilAlerte FROM produit WHERE nom = ?";
-
-	    String insertVente =
-	            "INSERT INTO vente (dateVente, quantite, montantTotal, nom_medicamment, idClient) " +
-	            "VALUES (?, ?, ?, ?, ?)";
-
-	    String updateStock =
-	            "UPDATE produit SET quantiteStock = quantiteStock - ? WHERE nom = ?";
-
-	    try (Connection con = DatabaseConnection.getConnection()) {
-
-	        con.setAutoCommit(false);
-
-	       
-	        int idClient;
-
-	        try (PreparedStatement ps = con.prepareStatement(checkClient)) {
-	            ps.setString(1, cinClient);
-	            ResultSet rs = ps.executeQuery();
-
-	            if (rs.next()) {
-	                idClient = rs.getInt("idClient");
-	            } else {
-	                try (PreparedStatement psInsert =
-	                             con.prepareStatement(insertClient, Statement.RETURN_GENERATED_KEYS)) {
-
-	                    psInsert.setString(1, "Client " + cinClient);
-	                    psInsert.setString(2, cinClient);
-	                    psInsert.executeUpdate();
-
-	                    ResultSet keys = psInsert.getGeneratedKeys();
-	                    keys.next();
-	                    idClient = keys.getInt(1);
-	                }
-	            }
-	        }
-
-	        
-	        double prix;
-	        int stock;
-	        int seuil;
-
-	        try (PreparedStatement ps = con.prepareStatement(checkProduit)) {
-	            ps.setString(1, nomMedicament);
-	            ResultSet rs = ps.executeQuery();
-
-	            if (!rs.next()) {
-	                throw new RuntimeException("Médicament introuvable !");
-	            }
-
-	            prix  = rs.getDouble("prixC");
-	            stock = rs.getInt("quantiteStock");
-	            seuil = rs.getInt("seuilAlerte");
-	        }
-
-	        if (stock < quantite) {
-	            throw new RuntimeException("Stock insuffisant !");
-	        }
-
-	        if (stock <= seuil) {
-	            System.out.println(" ALERTE : Stock sous le seuil !");
-	        }
-
-	        double montant = prix * quantite;
-
-	        try (PreparedStatement ps = con.prepareStatement(insertVente)) {
-	            ps.setDate(1, dateVente);
-	            ps.setInt(2, quantite);
-	            ps.setDouble(3, montant);
-	            ps.setString(4, nomMedicament);
-	            ps.setInt(5, idClient);
-	            ps.executeUpdate();
-	        }
-
-	        
-	        try (PreparedStatement ps = con.prepareStatement(updateStock)) {
-	            ps.setInt(1, quantite);
-	            ps.setString(2, nomMedicament);
-	            ps.executeUpdate();
-	        }
-
-	        con.commit();
-	        System.out.println(" Vente enregistrée avec succès");
-
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
+	    return st.executeQuery(sql);
 	}
 
-    public ResultSet getAllVentes() throws SQLException {
-        String sql = "SELECT * FROM vente ORDER BY dateVente DESC";
+    public Produit enregistrerVente(
+            Date dateVente,
+            String nomMedicament,
+            int quantite,
+            int idClient
+    ) throws SQLException, StockInsuffisantException {
+
         Connection con = DatabaseConnection.getConnection();
-        Statement st = con.createStatement();
-        return st.executeQuery(sql);
+        con.setAutoCommit(false);
+
+        try {
+
+            String sqlProduit =
+                    "SELECT prixC, quantiteStock, seuilAlerte " +
+                    "FROM produit WHERE nom = ?";
+
+            PreparedStatement psProduit = con.prepareStatement(sqlProduit);
+            psProduit.setString(1, nomMedicament);
+            ResultSet rsProduit = psProduit.executeQuery();
+
+            if (!rsProduit.next()) {
+                throw new SQLException("Produit introuvable");
+            }
+
+            double prixC = rsProduit.getDouble("prixC");
+            int stock = rsProduit.getInt("quantiteStock");
+            int seuil = rsProduit.getInt("seuilAlerte");
+
+            if (stock < quantite) {
+                throw new StockInsuffisantException("Stock insuffisant");
+            }
+
+            String sqlClient = "SELECT idClient FROM client WHERE idClient = ?";
+            PreparedStatement psClient = con.prepareStatement(sqlClient);
+            psClient.setInt(1, idClient);
+            ResultSet rsClient = psClient.executeQuery();
+
+            if (!rsClient.next()) {
+                throw new SQLException("Client inexistant");
+            }
+
+            String insertVente =
+                    "INSERT INTO vente (dateVente, quantite, montantTotal, nom_medicamment, idClient) " +
+                    "VALUES (?, ?, ?, ?, ?)";
+
+            PreparedStatement psVente = con.prepareStatement(insertVente);
+            psVente.setDate(1, dateVente);
+            psVente.setInt(2, quantite);
+            psVente.setDouble(3, quantite * prixC);
+            psVente.setString(4, nomMedicament);
+            psVente.setInt(5, idClient);
+            psVente.executeUpdate();
+
+            String updateStock =
+                    "UPDATE produit SET quantiteStock = quantiteStock - ? WHERE nom = ?";
+
+            PreparedStatement psUpdate = con.prepareStatement(updateStock);
+            psUpdate.setInt(1, quantite);
+            psUpdate.setString(2, nomMedicament);
+            psUpdate.executeUpdate();
+
+            con.commit();
+
+            Produit p = new Produit(
+                    nomMedicament,
+                    0,
+                    prixC,
+                    stock - quantite,
+                    seuil
+            );
+
+            return p;
+
+        } catch (Exception e) {
+            con.rollback();
+            throw e;
+        } finally {
+            con.close();
+        }
     }
 }
